2ed6b5e51babdf1d046193f779e3d77f3fa0cbea
MockAirplane700 <sizibamthandazo@yahoo.com>
2023-01-16 21:23:56 -0600

    fixed post

--- main.py
+++ main.py
@@ -59,9 +59,10 @@ class Paypal(Resource):
         data = request.get_data()
         string_data = data.decode('utf-8')
         if string_data.__contains__('\r\n\r\n'):
-            string_result = string_data.split('\r\n\r\n')[1].split('\r\n')[0]
+            string_result = (string_data.split('\r\n\r\n')[1].split('\r\n')[0]).rstrip('\"')
             print(string_result)
-            file.write(string_result.rstrip('\"'))
+            a = string_result
+            file.write(' <!DOCTYPE html>\n<html lang="en">\n<head>\n<meta name="viewport" content="width=device-width, initial-scale=1" />\n<meta http-equiv="X-UA-Compatible" content="IE=edge" />\n<title>PayPal Standard Payments Integration | Client Demo</title>\n</head>\n\n<body>\n<div id="paypal-button-container"></div>\n<!-- Sample PayPal credentials (client-id) are included -->\n<script src="https://www.paypal.com/sdk/js?client-id=test&currency=CAD&intent=capture&enable-funding=venmo" data-sdk-integration-source="integrationbuilder"></script>\n<script>\nconst paypalButtonsComponent = paypal.Buttons({\n// optional styling for buttons\n// https://developer.paypal.com/docs/checkout/standard/customize/buttons-style-guide/\nstyle: {\ncolor: "gold",\nshape: "rect",\nlayout: "vertical"\n},\n\n// set up the transaction\ncreateOrder: (data, actions) => {\n// pass in any options from the v2 orders create call:\n// https://developer.paypal.com/api/orders/v2/#orders-create-request-body\nconst createOrderPayload = {\npurchase_units: [\n{\namount: {\nvalue: %a\n}\n}\n]\n};\n\nreturn actions.order.create(createOrderPayload);\n},\n\n// finalize the transaction\nonApprove: (data, actions) => {\nconst captureOrderHandler = (details) => {\nconst payerName = details.payer.name.given_name;\nconsole.log("Transaction completed");\n};\n\nreturn actions.order.capture().then(captureOrderHandler);\n},\n\n// handle unrecoverable errors\nonError: (err) => {\nconsole.error("An error prevented the buyer from checking out with PayPal");\n}\n});\n\npaypalButtonsComponent\n.render("#paypal-button-container")\n.catch((err) => {\nconsole.error("PayPal Buttons failed to render");\n});\n</script>\n</body>\n</html> '%(a))
         file.close()
         return 'OK , 200'
 
